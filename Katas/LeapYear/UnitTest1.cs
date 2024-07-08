cff8b0a4a0c5ca8159b75bc132d06c2b95e2196d
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-08 05:46:03 +0200

    docs(LeapYear): document kata requirements

--- Katas/LeapYear/UnitTest1.cs
+++ Katas/LeapYear/UnitTest1.cs
@@ -1,5 +1,11 @@
 namespace LeapYear;
 
+// https://www.codurance.com/katas/leap-year
+// [] A year is a leap year if divisible by 4
+// [] A year is a leap year if divisible by 400
+// [] A year is not a leap year if not divisible by 4
+// [] A year is not a leap year if divisible by 100 but not by 400
+
 public class Tests
 {
     [Test]
