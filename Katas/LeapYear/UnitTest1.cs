964f3006a3fd021e469b02b1be25d36b3c14e80e
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-08 06:00:40 +0200

    feat(LeapYear): discard not divisible by 400 but by 100 years as leap years

--- Katas/LeapYear/UnitTest1.cs
+++ Katas/LeapYear/UnitTest1.cs
@@ -3,9 +3,9 @@ using FluentAssertions;
 namespace LeapYear;
 
 // https://www.codurance.com/katas/leap-year
-// [] A year is a leap year if divisible by 4
-// [] A year is a leap year if divisible by 400
-// [] A year is not a leap year if not divisible by 4
+// [x] A year is a leap year if divisible by 4
+// [x] A year is a leap year if divisible by 400
+// [x] A year is not a leap year if not divisible by 4
 // [] A year is not a leap year if divisible by 100 but not by 400
 
 public class Tests
@@ -23,12 +23,18 @@ public class Tests
         new SolarCalendar().IsLeap(year: 3).Should().BeFalse();
         new SolarCalendar().IsLeap(year: 7).Should().BeFalse();
     }
-    
+
     [Test]
     public void IsLeapYear_IfDivisible_By400()
     {
         new SolarCalendar().IsLeap(year: 400).Should().BeTrue();
         new SolarCalendar().IsLeap(year: 800).Should().BeTrue();
+    }
+
+    [Test]
+    public void IsNotLeapYear_IfDivisible_By100_ButNotBy400()
+    {
+        new SolarCalendar().IsLeap(year: 100).Should().BeFalse();
     }
 }
 
@@ -36,6 +42,8 @@ public readonly struct SolarCalendar
 {
     public bool IsLeap(int year)
     {
-        return year % 4 == 0;
+        return year < 100
+            ? year % 4 == 0
+            : year % 400 == 0;
     }
 }
