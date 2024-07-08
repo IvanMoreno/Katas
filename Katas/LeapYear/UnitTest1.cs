d4b7deb594d7368919d912ebf3ee8479689e9b2f
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-08 05:56:28 +0200

    refactor(LeapYear): rename IsLeapYear method to IsLeap

--- Katas/LeapYear/UnitTest1.cs
+++ Katas/LeapYear/UnitTest1.cs
@@ -13,20 +13,20 @@ public class Tests
     [Test]
     public void IsLeapYear_IfDivisible_By4()
     {
-        new SolarCalendar().IsLeapYear(4).Should().BeTrue();
-        new SolarCalendar().IsLeapYear(8).Should().BeTrue();
+        new SolarCalendar().IsLeap(year: 4).Should().BeTrue();
+        new SolarCalendar().IsLeap(year: 8).Should().BeTrue();
     }
-    
+
     [Test]
     public void IsNotLeapYear_IfNotDivisible_By4()
     {
-        new SolarCalendar().IsLeapYear(3).Should().BeFalse();
+        new SolarCalendar().IsLeap(year: 3).Should().BeFalse();
     }
 }
 
 public readonly struct SolarCalendar
 {
-    public bool IsLeapYear(int year)
+    public bool IsLeap(int year)
     {
         return year % 4 == 0;
     }
