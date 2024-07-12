46f90d3bd6735bdf6af435a6229f02776c47383b
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:35:29 +0200

    refactor(EmployeeReport): remove unused employee private constructor

--- Katas/Katas/EmployeeReport/EmployeeReportTests.cs
+++ Katas/Katas/EmployeeReport/EmployeeReportTests.cs
@@ -33,11 +33,10 @@ public struct Employee
     public readonly int Years;
     public readonly string Name;
 
-    Employee(int years) => Years = years;
     Employee(int years, string name)
     {
         Years = years;
-        this.Name = name;
+        Name = name;
     }
 
     public static Employee Hire(int years, string name) => new(years, name);
