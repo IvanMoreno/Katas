f741f189fa95a71ac8359f767694b8fab5b34c5e
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:53:58 +0200

    refactor(EmployeeReport): apply employee filter to staff

--- Katas/Katas/EmployeeReport/EmployeeReportTests.cs
+++ Katas/Katas/EmployeeReport/EmployeeReportTests.cs
@@ -40,14 +40,8 @@ public class EmployeeReportTests
 public class EmployeeFilter
 {
     readonly Func<IEnumerable<Employee>, IEnumerable<Employee>> filter;
+    EmployeeFilter(Func<IEnumerable<Employee>, IEnumerable<Employee>> filter) => this.filter = filter;
 
-    EmployeeFilter(Func<IEnumerable<Employee>, IEnumerable<Employee>> filter)
-    {
-        this.filter = filter;
-    }
-
-    public IEnumerable<Employee> ApplyFor(IEnumerable<Employee> staff) 
-        => staff.OrderByDescending(employee => employee.Name);
-
+    public IEnumerable<Employee> ApplyFor(IEnumerable<Employee> staff) => filter(staff);
     public static EmployeeFilter OrderByNamesDescending => new(staff => staff.OrderByDescending(x => x.Name));
 }
