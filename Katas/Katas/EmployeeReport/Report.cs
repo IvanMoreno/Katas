1e3fe21a22030c5e017782e87c2c996a07ff9078
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:40:36 +0200

    feat(EmployeeReport): capitalize employees names

--- Katas/Katas/EmployeeReport/Report.cs
+++ Katas/Katas/EmployeeReport/Report.cs
@@ -4,6 +4,14 @@ public class Report
 {
     public IEnumerable<Employee> ShowEligibleFrom(IEnumerable<Employee> allEmployees)
     {
-        return allEmployees.Where(x => x.Years >= 18).OrderBy(x => x.Name);
+        return allEmployees
+            .Where(x => x.Years >= 18)
+            .OrderBy(x => x.Name)
+            .Select(Capitalize);
+    }
+
+    Employee Capitalize(Employee who)
+    {
+        return Employee.Hire(who.Years, who.Name.ToUpper());
     }
 }
