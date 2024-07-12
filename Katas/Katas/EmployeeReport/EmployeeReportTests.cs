9a3943f3702069183140da0edd4b5e1382265ba6
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:28:34 +0200

    feat(EmployeeReport): select employees with 18 years or older

--- Katas/Katas/EmployeeReport/EmployeeReportTests.cs
+++ Katas/Katas/EmployeeReport/EmployeeReportTests.cs
@@ -7,5 +7,27 @@ namespace Katas.EmployeeReport;
 
 public class EmployeeReportTests
 {
-    
+    [Test]
+    public void SelectEmployees_With18YearsOrOlder()
+    {
+        var doc = new List<Employee> { Employee.Of(years: 17), Employee.Of(years: 18), Employee.Of(years: 54) };
+
+        new Report().ShowEligibleFrom(doc).Should().HaveCount(2);
+    }
+}
+
+public class Report
+{
+    public IEnumerable<Employee> ShowEligibleFrom(IEnumerable<Employee> allEmployees)
+    {
+        return allEmployees.Where(x => x.Years >= 18);
+    }
+}
+
+public struct Employee
+{
+    public readonly int Years;
+
+    Employee(int years) => Years = years;
+    public static Employee Of(int years) => new(years);
 }
