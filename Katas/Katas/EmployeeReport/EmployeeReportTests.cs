8b202f3c707dbae4d8038040102f2819a386cacb
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:29:59 +0200

    refactor(EmployeeReport): rename employee factory method to Hire

--- Katas/Katas/EmployeeReport/EmployeeReportTests.cs
+++ Katas/Katas/EmployeeReport/EmployeeReportTests.cs
@@ -1,16 +1,17 @@
 using FluentAssertions;
+using static Katas.EmployeeReport.Employee;
 
 namespace Katas.EmployeeReport;
 
 // https://codingdojo.org/kata/Employee-Report/
-// [] See the employees who are 18 years or older
+// [x] See the employees who are 18 years or older
 
 public class EmployeeReportTests
 {
     [Test]
     public void SelectEmployees_With18YearsOrOlder()
     {
-        var doc = new List<Employee> { Employee.Of(years: 17), Employee.Of(years: 18), Employee.Of(years: 54) };
+        var doc = new List<Employee> { Hire(years: 17), Hire(years: 18), Hire(years: 54) };
 
         new Report().ShowEligibleFrom(doc).Should().HaveCount(2);
     }
@@ -29,5 +30,5 @@ public struct Employee
     public readonly int Years;
 
     Employee(int years) => Years = years;
-    public static Employee Of(int years) => new(years);
+    public static Employee Hire(int years) => new(years);
 }
