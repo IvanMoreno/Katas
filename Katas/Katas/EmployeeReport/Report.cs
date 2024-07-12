b7f7580ad8788eb3587a0ac90e37dc86482d7bad
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 19:03:41 +0200

    refactor(EmployeeReport): convert report class constructor to primary constructor

--- Katas/Katas/EmployeeReport/Report.cs
+++ Katas/Katas/EmployeeReport/Report.cs
@@ -1,17 +1,10 @@
-using static Katas.EmployeeReport.Employee;
 using static Katas.EmployeeReport.EmployeeFilter;
 
 namespace Katas.EmployeeReport;
 
-public class Report
+public class Report(params EmployeeFilter[] filters)
 {
-    readonly IEnumerable<EmployeeFilter> filters;
-    public Report(IEnumerable<EmployeeFilter> filters) => this.filters = filters;
-
-    public static Report WithInitialFilters()
-    {
-        return new Report(new[] { Only18OrOlder, OrderByNames, CapitalizeNames, });
-    }
+    public static Report WithInitialFilters() => new(Only18OrOlder, OrderByNames, CapitalizeNames);
 
     public IEnumerable<Employee> EligibleFrom(IEnumerable<Employee> staff) 
         => filters.Aggregate(staff, (current, filter) => filter.ApplyFor(current));
