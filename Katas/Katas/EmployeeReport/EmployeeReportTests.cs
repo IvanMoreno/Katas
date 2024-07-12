b6e9121d78e5da84723449360f033381aa1e7002
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:19:45 +0200

    test(EmployeeReport): create employee report kata tests file

---
+++ Katas/Katas/EmployeeReport/EmployeeReportTests.cs
@@ -0,0 +1,11 @@
+using FluentAssertions;
+
+namespace Katas.EmployeeReport;
+
+// https://codingdojo.org/kata/Employee-Report/
+// [] See the employees who are 18 years or older
+
+public class EmployeeReportTests
+{
+    
+}
