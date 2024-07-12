31fd7df8d074d87ac83c57188b55967a0d342574
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-12 18:59:29 +0200

    refactor(EmployeeReport): include employee filters in report

--- Katas/Katas/EmployeeReport/Report.cs
+++ Katas/Katas/EmployeeReport/Report.cs
@@ -1,12 +1,16 @@
 using static Katas.EmployeeReport.Employee;
+using static Katas.EmployeeReport.EmployeeFilter;
 
 namespace Katas.EmployeeReport;
 
 public class Report
 {
+    readonly IEnumerable<EmployeeFilter> filters;
+    public Report(IEnumerable<EmployeeFilter> filters) => this.filters = filters;
+
     public static Report WithInitialFilters()
     {
-        return new Report();
+        return new Report(new[] { Only18OrOlder, OrderByNames, CapitalizeNames, });
     }
 
     public IEnumerable<Employee> EligibleFrom(IEnumerable<Employee> staff)
