92bad9ac6157a227687889a1ed774f994d6f77c5
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-15 18:02:57 +0200

    docs(RomanNumerals): document initial requirements

--- Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
+++ Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
@@ -1,8 +1,15 @@
 namespace Katas.RomanNumerals;
 
 // https://www.codurance.com/katas/roman-numerals
+// [] 1 --> I
+// [] 2 and 3 --> II and III
+// [] 4 --> IV (there cannot exist a sequence of four 'I')
+// [] 5 --> V
+// [] 6, 7 and 8 --> Append 'I' for each unit after five (VI, VII, VIII)
+// [] 9 --> IX an 'I' is also prepended to avoid a sequence of four 'I'
+// [] 10 --> X
 
 public class RomanNumeralsTests
 {
-        
+    
 }
