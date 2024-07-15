e5b30ff4cd6a5fb4da4bb30315b8b96aa002734b
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-15 18:16:33 +0200

    refactor(RomanNumerals): extract RomanTranslator class

--- Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
+++ Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
@@ -44,23 +44,3 @@ public class RomanNumeralsTests
         new RomanTranslator().Translate(30).Should().Be("XXX");
     }
 }
-
-public class RomanTranslator
-{
-    public string Translate(int howMuch)
-    {
-        if (howMuch < 0)
-            throw new ArgumentException("The Romans did not use negative numbers");
-        if (howMuch == 0)
-            throw new ArgumentException("The Romans did not use 0");
-
-        if (howMuch == 1)
-            return "I";
-        if (howMuch < 10)
-            return "I" + Translate(howMuch - 1);
-        if (howMuch == 10)
-            return "X";
-
-        return "X" + Translate(howMuch - 10);
-    }
-}
---
+++ Katas/Katas/RomanNumerals/RomanTranslator.cs
@@ -0,0 +1,21 @@
+namespace Katas.RomanNumerals;
+
+public class RomanTranslator
+{
+    public string Translate(int howMuch)
+    {
+        if (howMuch < 0)
+            throw new ArgumentException("The Romans did not use negative numbers");
+        if (howMuch == 0)
+            throw new ArgumentException("The Romans did not use 0");
+
+        if (howMuch == 1)
+            return "I";
+        if (howMuch < 10)
+            return "I" + Translate(howMuch - 1);
+        if (howMuch == 10)
+            return "X";
+
+        return "X" + Translate(howMuch - 10);
+    }
+}
