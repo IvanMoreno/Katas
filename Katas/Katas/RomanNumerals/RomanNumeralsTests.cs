b46898debf0fd67c607266cd8665b78b032ef2a5
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-15 18:18:25 +0200

    test(RomanNumerals): append nearest symbol to remaining number after subtraction

--- Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
+++ Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
@@ -9,7 +9,8 @@ namespace Katas.RomanNumerals;
 // [] 5 --> V
 // [] 6, 7 and 8 --> Append 'I' for each unit after five (VI, VII, VIII)
 // [] 9 --> IX an 'I' is also prepended to avoid a sequence of four 'I'
-// [] 10 --> X
+// [x] 10 --> X
+// [x] 'X' works as 'I'
 
 public class RomanNumeralsTests
 {
@@ -42,5 +43,12 @@ public class RomanNumeralsTests
     {
         new RomanTranslator().Translate(20).Should().Be("XX");
         new RomanTranslator().Translate(30).Should().Be("XXX");
+    }
+
+    [Test]
+    public void Append_NearestSymbol_ToRemainingNumber_AfterSubtraction()
+    {
+        new RomanTranslator().Translate(22).Should().Be("XXII");
+        new RomanTranslator().Translate(13).Should().Be("XIII");
     }
 }
