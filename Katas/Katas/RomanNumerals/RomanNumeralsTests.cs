a506a3ffb13329f8a802e9d7003045ec8889d615
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-07-15 18:09:38 +0200

    feat(RomanNumerals): append three 'I' to translate 3

--- Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
+++ Katas/Katas/RomanNumerals/RomanNumeralsTests.cs
@@ -4,7 +4,7 @@ namespace Katas.RomanNumerals;
 
 // https://www.codurance.com/katas/roman-numerals
 // [x] 1 --> I
-// [] 2 and 3 --> II and III
+// [x] 2 and 3 --> II and III
 // [] 4 --> IV (there cannot exist a sequence of four 'I')
 // [] 5 --> V
 // [] 6, 7 and 8 --> Append 'I' for each unit after five (VI, VII, VIII)
@@ -24,6 +24,12 @@ public class RomanNumeralsTests
     {
         new RomanTranslator().Translate(2).Should().Be("II");
     }
+
+    [Test]
+    public void Translate_3()
+    {
+        new RomanTranslator().Translate(3).Should().Be("III");
+    }
 }
 
 public class RomanTranslator
@@ -36,6 +42,7 @@ public class RomanTranslator
             throw new ArgumentException("The Romans did not use 0");
 
         if (howMuch == 2) return "II";
+        if (howMuch == 3) return "III";
         return "I";
     }
 }
