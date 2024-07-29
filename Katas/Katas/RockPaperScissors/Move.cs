0bd4745f3a9c8703d46a71fe0c21e74d2e08ec54
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-07-29 18:24:02 +0200

    refactor(RockPaperScissors): replace switch with winner map

--- Katas/Katas/RockPaperScissors/Move.cs
+++ Katas/Katas/RockPaperScissors/Move.cs
@@ -2,19 +2,19 @@ namespace Katas.RockPaperScissors;
 
 public record Move
 {
+    static Dictionary<Move, Move> WinnerOf => new()
+    {
+        { Rock, Paper },
+        { Paper, Scissors },
+        { Scissors, Rock }
+    };
+
     readonly string figure;
     Move(string figure) => this.figure = figure;
 
-    public bool Beats(Move opponent) 
-        => figure switch
-        {
-            "Rock" => opponent == Scissors,
-            "Paper" => opponent == Rock,
-            "Scissors" => opponent == Paper,
-            _ => throw new NotImplementedException($"{figure}'s opponent is not implemented")
-        };
-
+    public bool Beats(Move opponent) => WinnerOf[opponent] == this;
     public override string ToString() => figure;
+    
     public static Move Rock => new("Rock");
     public static Move Paper => new("Paper");
     public static Move Scissors => new("Scissors");
