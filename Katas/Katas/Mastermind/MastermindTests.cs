5c5ffe06dff749e54e32fb7909585e3589460425
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-07-29 18:56:03 +0200

    contract: ensure both guess and secret have the same length

--- Katas/Katas/Mastermind/MastermindTests.cs
+++ Katas/Katas/Mastermind/MastermindTests.cs
@@ -40,6 +40,9 @@ public class CodeMaker(params Color[] secret)
 {
     public GuessResult Guess(IEnumerable<Color> guess)
     {
+        if (guess.Count() != secret.Length)
+            throw new ArgumentException("Guess must have same length than secret");
+        
         return new GuessResult(correctGuesses: secret.Zip(guess).Count(x => x.First == x.Second));
     }
 }
