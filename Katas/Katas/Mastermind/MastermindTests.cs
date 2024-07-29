382c01fa6c9a7b37185ff190615c9f5c8bac9e3e
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-07-29 18:43:18 +0200

    docs(Mastermind): document Mastermind's requirements

---
+++ Katas/Katas/Mastermind/MastermindTests.cs
@@ -0,0 +1,13 @@
+namespace Katas.Mastermind;
+
+// https://codingdojo.org/kata/Mastermind/
+// [] Return the number of correct colors placed in the correct position
+//   [] In order for this to happen, there must exist a secret code
+//   [] It is necessary to know where a color is placed
+//   [] A secret code is the relation of a color with its position (we could use the index of the array for the moment)
+// [] Return the number of correct but misplaced colors
+
+public class MastermindTests
+{
+    
+}
