ce324887101a33373a469a78d7f48671847a2ce5
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-07 18:17:40 +0200

    docs(SmartFridge): document kata requirements

--- Katas/Katas/SmartFridge/SmartFridgeTests.cs
+++ Katas/Katas/SmartFridge/SmartFridgeTests.cs
@@ -1,7 +1,19 @@
 namespace Katas.SmartFridge;
 
 // https://www.codurance.com/katas/smart-fridge
-// 
+// [] Display the items of the Fridge:
+//    [] Each item have an expiration date
+//    [] Each item can be opened or closed when introduced in the fridge, this cannot be changed
+//    [] An item is expired if its expiration date matches today's date
+//    [] A non expired item will be displayed as 'item.name: n day(s) remaining'
+//    [] An expired item will be displayed as 'EXPIRED: item.name'
+//    [] Expired items are displayed before non expired items
+// [] Add an item to the fridge
+// [] Opening the fridge degrades all items inside the fridge
+//    [] If the item is opened, it is degraded by 5 hours
+//    [] If the item is closed, it is degraded by 1 hour
+
+// Dates are an important part of this kata, every operation performed in the fridge must be tracked.
 
 public class SmartFridgeTests
 {
