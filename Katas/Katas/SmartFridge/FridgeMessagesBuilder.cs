c69dcc8af5c66dd64efd3f1244ec1a883e24a605
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-11 11:59:04 +0200

    test(SmartFridge): extract FridgeMessagesBuilder

---
+++ Katas/Katas/SmartFridge/FridgeMessagesBuilder.cs
@@ -0,0 +1,13 @@
+namespace Katas.SmartFridge;
+
+public static class FridgeMessagesBuilder
+{
+    public static string Expired(this string product) => $"EXPIRED: {product}";
+    public static string ExpiringInDays(this string product, int days)
+    {
+        return $"{product}: {days} day(s) remaining";
+    }
+
+    public static string ALettuce => Supermarket.Lettuce(new DateTime()).Name;
+    public static string ATomato => Supermarket.Tomato(new DateTime()).Name;
+}
--- Katas/Katas/SmartFridge/Supermarket.cs
+++ Katas/Katas/SmartFridge/Supermarket.cs
@@ -1,18 +1,4 @@
-using static Katas.SmartFridge.Supermarket;
-
 namespace Katas.SmartFridge;
-
-public static class FridgeMessagesBuilder
-{
-    public static string Expired(this string product) => $"EXPIRED: {product}";
-    public static string ExpiringInDays(this string product, int days)
-    {
-        return $"{product}: {days} day(s) remaining";
-    }
-
-    public static string ALettuce => Lettuce(new DateTime()).Name;
-    public static string ATomato => Tomato(new DateTime()).Name;
-}
 
 public static class Supermarket
 {
