3ba6984327729755a264c2e660132027b11b1893
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-11 13:09:03 +0200

    refactor(SmartFridge): inherit item from event

--- Katas/Katas/SmartFridge/Item.cs
+++ Katas/Katas/SmartFridge/Item.cs
@@ -5,7 +5,7 @@ public record Event
     
 }
 
-public record Item(string Name, DateTime ExpirationDate)
+public record Item(string Name, DateTime ExpirationDate) : Event
 {
     public DateTime AdditionDate { get; init; }
     public bool Opened { get; init; }
