b9608d40c66df88782d031026bbb01aa0a0b2417
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-11 13:26:40 +0200

    refactor(SmartFridge): simplify item degradation logic

--- Katas/Katas/SmartFridge/Fridge.cs
+++ Katas/Katas/SmartFridge/Fridge.cs
@@ -30,14 +30,9 @@ public class Fridge
     int DaysUntilExpiration(Item item)
         => (item.ExpirationDate - today - DegradationByAirExposure(item)).Days - 1;
 
-    TimeSpan DegradationByAirExposure(Item item)
-        => Openings.Aggregate(Zero,
-            (totalDegradation, opening) => totalDegradation + HowMuchDegrades(item, opening));
-
-    static TimeSpan HowMuchDegrades(Item anItem, OpenedFridge at)
-        => at.When >= anItem.AdditionDate ? DegradationTime(anItem) : FromHours(0);
-
-    static TimeSpan DegradationTime(Item anItem) => anItem.Opened ? FromHours(5) : FromHours(1);
+    TimeSpan DegradationByAirExposure(Item item)=> OpeningsAfter(item) * DegradationOf(item);
+    int OpeningsAfter(Item item) => Openings.Count(x => x.When >= item.AdditionDate);
+    static TimeSpan DegradationOf(Item anItem) => anItem.Opened ? FromHours(5) : FromHours(1);
     public Fridge Put(Item item) => new(today, allEvents.Append(item with { AdditionDate = today }));
     public Fridge OpenDoor() => new(today, allEvents.Append(new OpenedFridge(today)));
     public Fridge Pass(TimeSpan howMuchTime) => new(today + howMuchTime, allEvents);
