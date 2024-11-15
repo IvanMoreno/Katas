ec252998363ccb4a3cf4e8b444b714964b44ee4f
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-11-15 18:36:27 +0100

    refactor(Stack): create new node using with expression

--- Katas/Katas/Stack/Node.cs
+++ Katas/Katas/Stack/Node.cs
@@ -3,20 +3,11 @@ namespace Katas.Stack;
 public record Node<T>
 {
     public readonly T Value;
-    public Node<T>? Child { get; private set; }
+    public Node<T>? Child { get; private init; }
     public bool HasChild => Child != null;
     public int ChildrenCount => HasChild ? 1 + Child!.ChildrenCount : 0;
 
     Node(T value) => Value = value;
-
-    public static Node<T> From(T value)
-    {
-        return new(value);
-    }
-
-    public Node<T> FatherOf(Node<T> child)
-    {
-        Child = child;
-        return this;
-    }
+    public static Node<T> From(T value) => new(value);
+    public Node<T> FatherOf(Node<T> child) => this with { Child = child };
 }
