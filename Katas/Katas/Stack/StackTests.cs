7020aff8c02f1850bf83a8565cea935baee29886
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-11-15 17:28:06 +0100

    docs(Stack): update requirements list progress

--- Katas/Katas/Stack/StackTests.cs
+++ Katas/Katas/Stack/StackTests.cs
@@ -3,10 +3,10 @@ using FluentAssertions;
 namespace Katas.Stack;
 
 // https://www.codurance.com/katas/stack
-// [] Push - Add an element to the top of the stack    
-// [] Pop - Remove an element from the top of the stack, returning it
+// [x] Push - Add an element to the top of the stack    
+// [x] Pop - Remove an element from the top of the stack, returning it
 // [] Empty check - Check if the stack is empty or not
-// [] Size - Count of the elements in the stack
+// [x] Size - Count of the elements in the stack
 // [] Peek - Check the top of the stack without popping
 
 public class StackTests
