0c90b974474c58243a7b33c21f8dd1d89a568132
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-11-15 18:16:43 +0100

    docs(Stack): update requirements list progress

--- Katas/Katas/Stack/NodeTests.cs
+++ Katas/Katas/Stack/NodeTests.cs
@@ -6,7 +6,7 @@ namespace Katas.Stack;
 // Extra(Stack): Now without using any kind of collections
 // My approach: create my own type Node that can store values and child nodes.
 // [x] Retrieve value from Node
-// [] Retrieve child from Node
+// [x] Retrieve child from Node
 // [] Children count
 // [x] HasChild
 
