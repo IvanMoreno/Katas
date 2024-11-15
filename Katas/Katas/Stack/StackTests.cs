1af1dab04d7d2b0e63ae41293b6379e0bc8a46af
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-11-15 16:51:59 +0100

    docs(Stack): write down kata requirements

---
+++ Katas/Katas/Stack/StackTests.cs
@@ -0,0 +1,13 @@
+namespace Katas.Stack;
+
+// https://www.codurance.com/katas/stack
+// [] Push - Add an element to the top of the stack    
+// [] Pop - Remove an element from the top of the stack, returning it
+// [] Empty check - Check if the stack is empty or not
+// [] Size - Count of the elements in the stack
+// [] Peek - Check the top of the stack without popping
+
+public class StackTests
+{
+    
+}
