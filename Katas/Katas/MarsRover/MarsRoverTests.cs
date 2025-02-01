02b0dfc3eb46c84c713edbb2fcc9c9034b21c974
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-02-01 07:47:18 +0100

    feat: move rover forward

---
+++ Katas/Katas/MarsRover/MarsRoverTests.cs
@@ -0,0 +1,27 @@
+using FluentAssertions;
+
+namespace Katas.MarsRoverKata;
+
+// [] Mover Rover hacia la dirección a la que mira
+// [] Girar Rover
+// [] Dar la vuelta a la cuadrícula
+// [] Detectar obstáculo
+//   [] Informar de encontrarse el obstáculo
+
+public class MarsRoverTests
+{
+    [Test]
+    public void MoveForward()
+    {
+        new MarsRover().Execute("M").Should().Be("0:1:N");
+    }
+}
+
+public class MarsRover
+
+{
+    public string Execute(string command)
+    {
+        return "0:1:N";
+    }
+}
