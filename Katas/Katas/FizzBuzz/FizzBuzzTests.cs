79632914b1070213b28d5eed2ac2b576f897e492
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-01 19:41:16 +0100

    feat: return same number for non multiple of three or five

---
+++ Katas/Katas/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+
+// https://www.codurance.com/katas/fizzbuzz
+// [] If the number is a multiple of three, return the string "Fizz".
+// [] If the number is a multiple of five, return the string "Buzz".
+// [] If the number is a multiple of both three and five, return the string "FizzBuzz".
+// [] No negative numbers
+
+public class FizzbuzzTests
+{
+    [Test]
+    public void NonMultipleOfThreeOrFiveReturnsSameNumber()
+    {
+        new FizzBuzz().Of(1).Should().Be("1");
+    }
+}
+
+public class FizzBuzz   
+{
+    public string Of(int i)
+    {
+        return "1";
+    }
+}
