eedc53039c49f5ebef19d0bf34c1b3b60a455d75
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-01 20:44:25 +0100

    refactor: remove unused method

--- Katas/Katas/FizzBuzz/Divisors.cs
+++ Katas/Katas/FizzBuzz/Divisors.cs
@@ -11,20 +11,6 @@ public readonly struct Divisors(Dictionary<NaturalNumber, Word> divisorToWord)
         return false;
     }
 
-    public Word WordOfDivisorOf(NaturalNumber number)
-    {
-        if (!ExistsDivisorFor(number))
-            throw new ArgumentException("Number not divisible");
-                
-        foreach (var (divisor, word) in divisorToWord)
-        {
-            if (number.IsDivisibleBy(divisor))
-                return word;
-        }
-
-        throw new Exception();
-    }
-
     public IEnumerable<Word> DivisorWords(NaturalNumber number)
     {
         if (!ExistsDivisorFor(number))
