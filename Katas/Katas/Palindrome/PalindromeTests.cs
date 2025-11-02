eea6f74fab5b7956a4254786af43f1451b6d6b32
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-02 16:48:37 +0100

    refactor: extract palindrome candidate notion

--- Katas/Katas/Palindrome/PalindromeTests.cs
+++ Katas/Katas/Palindrome/PalindromeTests.cs
@@ -20,8 +20,13 @@ public class PalindromeTests
 
 public class PalindromeDetector
 {
-    public bool IsPalindrome(string word)
+    public bool IsPalindrome(PalindromeCandidate word)
     {
         return true;
     }
+}
+
+public readonly struct PalindromeCandidate(string word)
+{
+    public static implicit operator PalindromeCandidate(string word) => new(word);
 }
