9a8a3e1ddb2f95ec8e91b3628a9345b793cc0d8f
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-02 17:00:59 +0100

    feat: return true for number palindromes

--- Katas/Katas/Palindrome/PalindromeCandidate.cs
+++ Katas/Katas/Palindrome/PalindromeCandidate.cs
@@ -1,11 +1,12 @@
 public readonly struct PalindromeCandidate(string word)
 {
     public static implicit operator PalindromeCandidate(string word) => new(word);
+    public static implicit operator PalindromeCandidate(int number) => new(number.ToString());
 
     public PalindromeCandidate Reverse() => word.Reverse().Aggregate(string.Empty, (acc, letter) => acc + letter);
 
     public override string ToString() => word;
 
     public static PalindromeCandidate FromWord(string word) => word;
-    public static PalindromeCandidate FromNumber(int number) => number.ToString();
+    public static PalindromeCandidate FromNumber(int number) => number;
 }
--- Katas/Katas/Palindrome/PalindromeTests.cs
+++ Katas/Katas/Palindrome/PalindromeTests.cs
@@ -3,19 +3,23 @@ using static PalindromeCandidate;
 
 // https://hackmd.io/@evalverde/B1ITM1-GJe
 // [x] Return true when an input is a palindrome
-// [] Handle both strings and numbers
+// [x] Handle both strings and numbers
 // [] Ignore spaces
 // [] Ignore punctuation
 // [] Ignore differences in letter case
 
 public class PalindromeTests
 {
+    static readonly PalindromeCandidate[] palindromes = { "aba", "cac", "opopo", 121, 4334 };
+    
     [Test]
     public void SingleLetterIsAlwaysPalindrome()
     {
         new PalindromeDetector().IsPalindrome("a").Should().BeTrue();
         new PalindromeDetector().IsPalindrome("b").Should().BeTrue();
         new PalindromeDetector().IsPalindrome("c").Should().BeTrue();
+        new PalindromeDetector().IsPalindrome(2).Should().BeTrue();
+        new PalindromeDetector().IsPalindrome(8).Should().BeTrue();
     }
 
     [Test]
@@ -37,10 +41,9 @@ public class PalindromeTests
         new PalindromeDetector().IsPalindrome("be").Should().BeFalse();
     }
 
-    [Test]
-    public void ReturnTrueWheWordIsPalindrome()
+    [TestCaseSource(nameof(palindromes))]
+    public void ReturnTrueWhenPalindrome(PalindromeCandidate candidate)
     {
-        new PalindromeDetector().IsPalindrome("acaca").Should().BeTrue();
-        new PalindromeDetector().IsPalindrome("ocoloco").Should().BeTrue();
+        new PalindromeDetector().IsPalindrome(candidate).Should().BeTrue();
     }
 }
