733e5d57cfb9701c035c8ee4ce6c7ab319c87d52
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-03 05:47:44 +0100

    refactor: remove unnecessary code

--- Katas/Katas/ManhattanDistance/ManhattanDistance.cs
+++ Katas/Katas/ManhattanDistance/ManhattanDistance.cs
@@ -2,9 +2,6 @@ public class ManhattanDistance
 {
     public static int Between(Point a, Point b)
     {
-        if (a.Equals(Point.Point2D(1, 1)) && b.Equals(Point.Point2D(3, 1)))
-            return 2;
-        
         return b.ElementSum() - a.ElementSum();
     }
 }
