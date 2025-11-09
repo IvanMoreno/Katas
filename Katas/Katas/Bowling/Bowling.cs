c1d0c2aacdfa4648012f9da407071ab2a5a12141
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-09 18:40:40 +0100

    refactor: extract pins struct

--- Katas/Katas/Bowling/Bowling.cs
+++ Katas/Katas/Bowling/Bowling.cs
@@ -19,21 +19,3 @@ public class Bowling
         return new Bowling();
     }
 }
-
-public readonly struct Pins
-{
-    readonly int howMany;
-
-    Pins(int howMany)
-    {
-        if (howMany > 10)
-            throw new ArgumentException("Cannot have more than 10 pins");
-        
-        this.howMany = howMany;
-    }
-
-    public static implicit operator int(Pins pins) => pins.howMany;
-    public static implicit operator Pins(int pins) => new(pins);
-
-    public static Pins One => 1;
-}
