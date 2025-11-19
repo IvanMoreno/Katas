a1eb5847917324959789303c1c63f47d9047a8d4
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-19 06:08:32 +0100

    refactor: extract word notion

--- Katas/Katas/Anagram/AnagramTests.cs
+++ Katas/Katas/Anagram/AnagramTests.cs
@@ -15,16 +15,26 @@ public class AnagramTests
     [Test]
     public void IsAnagram()
     {
-        "ab".IsAnagramOf("ba").Should().BeTrue();
-        "aba".IsAnagramOf("ba").Should().BeFalse();
+        new Word("ab").IsAnagramOf(new Word("ba")).Should().BeTrue();
+        new Word("aba").IsAnagramOf(new Word("ba")).Should().BeFalse();
     }
 }
 
-public static class Word
+public readonly struct Word
 {
-    public static bool IsAnagramOf(this string wordA, string wordB)
+    readonly string content;
+
+    public Word(string content)
     {
-        if (wordA.Length != wordB.Length)
+        if (string.IsNullOrEmpty(content))
+            throw new ArgumentException("word cannot be empty");
+        
+        this.content = content;
+    }
+
+    public bool IsAnagramOf(Word otherWord)
+    {
+        if (content.Length != otherWord.content.Length)
             return false;
         
         return true;
