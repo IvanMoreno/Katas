884ecbde9709be4912f0bd84a3694723b0567560
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-11-19 06:08:55 +0100

    refactor: extract file

--- Katas/Katas/Anagram/AnagramTests.cs
+++ Katas/Katas/Anagram/AnagramTests.cs
@@ -19,24 +19,3 @@ public class AnagramTests
         new Word("aba").IsAnagramOf(new Word("ba")).Should().BeFalse();
     }
 }
-
-public readonly struct Word
-{
-    readonly string content;
-
-    public Word(string content)
-    {
-        if (string.IsNullOrEmpty(content))
-            throw new ArgumentException("word cannot be empty");
-        
-        this.content = content;
-    }
-
-    public bool IsAnagramOf(Word otherWord)
-    {
-        if (content.Length != otherWord.content.Length)
-            return false;
-        
-        return true;
-    }
-}
---
+++ Katas/Katas/Anagram/Word.cs
@@ -0,0 +1,22 @@
+namespace Katas.Anagram;
+
+public readonly struct Word
+{
+    readonly string content;
+
+    public Word(string content)
+    {
+        if (string.IsNullOrEmpty(content))
+            throw new ArgumentException("word cannot be empty");
+        
+        this.content = content;
+    }
+
+    public bool IsAnagramOf(Word otherWord)
+    {
+        if (content.Length != otherWord.content.Length)
+            return false;
+        
+        return true;
+    }
+}
