d3fc8d5c5dcb745570b27e7e50307ba70aded24a
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-05-30 11:17:34 +0200

    refactor: extract movement superclass

--- Katas/Katas/RockPaperScissors2/Rock.cs
+++ Katas/Katas/RockPaperScissors2/Rock.cs
@@ -1,16 +1,18 @@
 namespace Katas.RockPaperScissors2;
 
-public class Rock {
-    readonly object winsAgainst;
-    
-    public Rock(object winsAgainst) {
-        this.winsAgainst = winsAgainst;
-    }
+public class Movement {
+    protected object winsAgainst;
 
     public Outcome Against(object other) {
         return winsAgainst.GetType() == other.GetType() ? Outcome.Win 
             : other.GetType() == GetType() ? Outcome.Tie 
             : Outcome.Lose;
+    }
+}
+
+public class Rock : Movement {
+    public Rock(object winsAgainst) {
+        this.winsAgainst = winsAgainst;
     }
 
     public static Rock Create() {
