62d26bec29ff950d974a240bab4b3e1d7721c629
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-05-30 11:28:38 +0200

    refactor: use figure instead of object

--- Katas/Katas/RockPaperScissors2/Movement.cs
+++ Katas/Katas/RockPaperScissors2/Movement.cs
@@ -1,25 +1,25 @@
 namespace Katas.RockPaperScissors2;
 
 public class Movement {
-    readonly object winsAgainst;
-    readonly object figure;
+    readonly Figure winsAgainst;
+    readonly Figure figure;
 
-    Movement(object winsAgainst, object figure) {
+    Movement(Figure winsAgainst, Figure figure) {
         this.winsAgainst = winsAgainst;
         this.figure = figure;
     }
 
     public Outcome Against(Movement other) {
-        return winsAgainst.GetType() == other.figure.GetType() ? Outcome.Win 
-            : figure.GetType() == other.figure.GetType() ? Outcome.Tie 
+        return winsAgainst.Equals(other.figure) ? Outcome.Win 
+            : figure.Equals(other.figure) ? Outcome.Tie 
             : Outcome.Lose;
     }
     
     public static Movement Rock() {
-        return new Movement(winsAgainst: new Scissors(), figure: new Rock());
+        return new Movement(winsAgainst: Figure.Scissors, figure: Figure.Rock);
     }
     
     public static Movement Scissors() {
-        return new Movement(winsAgainst: new Scissors(), figure: new Scissors());
+        return new Movement(winsAgainst: Figure.Scissors, figure: Figure.Scissors);
     }
 }
