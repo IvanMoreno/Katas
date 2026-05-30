f8ce406a47d9b2f4beadaa811d271c1eb2d83350
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-05-30 11:19:31 +0200

    refactor: import static movement methods

--- Katas/Katas/RockPaperScissors2/RockPaperScissorsTests.cs
+++ Katas/Katas/RockPaperScissors2/RockPaperScissorsTests.cs
@@ -1,21 +1,22 @@
 using FluentAssertions;
+using static Katas.RockPaperScissors2.Movement;
 
 namespace Katas.RockPaperScissors2;
 
 public class RockPaperScissorsTests {
     [Test]
     public void RockBeatsScissors() {
-        Movement.Rock().Against(Scissors.Create()).Should().Be(Outcome.Win);
+        Rock().Against(Scissors.Create()).Should().Be(Outcome.Win);
     }
 
     [Test]
     public void ScissorsLoosesAgainstRock() {
-        Scissors.Create().Against(Movement.Rock()).Should().Be(Outcome.Lose);
+        Scissors.Create().Against(Rock()).Should().Be(Outcome.Lose);
     }
 
     [Test]
     public void RockTiesAgainstItself() {
-        Movement.Rock().Against(Movement.Rock()).Should().Be(Outcome.Tie);
+        Rock().Against(Rock()).Should().Be(Outcome.Tie);
     }
 
     [Test]
