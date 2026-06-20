a0fd5e1a366d30e21254de0675f27e4fce08afff
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 21:58:04 +0200

    refactor: rearrange variables for higher cohesion

--- Katas/Katas/Potter/BundleSlicer.cs
+++ Katas/Katas/Potter/BundleSlicer.cs
@@ -5,16 +5,15 @@ public class BundleSlicer {
         if (books.Count == 1)
             return [new PotterBookBundle(books)];
         
-        var remainingBooks = new List<PotterBook>(books);
         var bundle = books.Distinct();
+        var result = new List<PotterBookBundle> {
+            new PotterBookBundle(bundle),
+        };
         
+        var remainingBooks = new List<PotterBook>(books);
         foreach (var book in bundle) {
             remainingBooks.Remove(book);
         }
-
-        var result = new List<PotterBookBundle>() {
-            new PotterBookBundle(bundle),
-        };
         
         if (remainingBooks.Count > 0)
             result.AddRange(BundleFrom(remainingBooks));
