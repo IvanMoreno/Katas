10d66bf2212dc668badef8a8347d3755525581ab
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 20:00:47 +0200

    refactor: reformulate discount expression

--- Katas/Katas/Potter/Basket.cs
+++ Katas/Katas/Potter/Basket.cs
@@ -19,13 +19,13 @@ public class Basket {
         }
         
         if (books.Count == 3) {
-            var have10Discount = !books[0].Equals(books[1]) && !books[0].Equals(books[2]);
+            var have10Discount = books.Distinct().Count() == 3;
             if (have10Discount) {
                 var bundlePrice = new Price(books.Count * 8);
                 return bundlePrice.Off(new Discount(10));
             }
             
-            var have5Discount = !books[0].Equals(books[1]) || !books[0].Equals(books[2]);
+            var have5Discount = books.Distinct().Count() == 2;
             if (have5Discount) {
                 var bundlePrice = new Price(2 * 8);
                 return bundlePrice.Off(new Discount(5)) + new Price(8);
