61296fa32ebdbb64d5782817906aa65b972c1550
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 19:39:43 +0200

    chore: discount precondition

--- Katas/Katas/Potter/Discount.cs
+++ Katas/Katas/Potter/Discount.cs
@@ -4,6 +4,9 @@ public readonly struct Discount {
     readonly float percent;
 
     public Discount(int percent) {
+        if (percent < 1 || percent > 100)
+            throw new ArgumentException("Discount must be between 1 and 100");
+        
         this.percent = percent * 0.01f;
     }
 
