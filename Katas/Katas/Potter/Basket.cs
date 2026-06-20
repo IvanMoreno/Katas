9e4e7ecc26d6beda616e0aebeb15ff2af8b6bfa0
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 20:51:30 +0200

    refactor: extract bundle notion

--- Katas/Katas/Potter/Basket.cs
+++ Katas/Katas/Potter/Basket.cs
@@ -17,15 +17,39 @@ public class Basket {
     }
 
     public Price Price() {
-        var bundleSize = books.Distinct().Count();
+        var bundle = new PotterBookBundle(books.Distinct());
 
-        var bundlePrice = new Price(bundleSize * 8).Off(DiscountFor(bundleSize));
+        var bundlePrice = bundle.Price;
         
-        var nonBundledBooksPrice = new Price((books.Count - bundleSize) * 8);
+        var nonBundledBooksPrice = new Price((books.Count - bundle.Size) * 8);
         
         return bundlePrice + nonBundledBooksPrice;
     }
 
+    static Discount DiscountFor(int bundleSize) {
+        return bundleSizeToDiscount[bundleSize];
+    }
+}
+
+public class PotterBookBundle {
+    static readonly Dictionary<int, Discount> bundleSizeToDiscount = new() {
+        { 0, new Discount(0) },
+        { 1, new Discount(0) },
+        { 2, new Discount(5) },
+        { 3, new Discount(10) },
+        { 4, new Discount(20) },
+        { 5, new Discount(25) }
+    };
+    
+    readonly IEnumerable<PotterBook> books;
+
+    public Price Price => new Price(Size * 8).Off(DiscountFor(Size));
+    public int Size => books.Count();
+
+    public PotterBookBundle(IEnumerable<PotterBook> books) {
+        this.books = books;
+    }
+    
     static Discount DiscountFor(int bundleSize) {
         return bundleSizeToDiscount[bundleSize];
     }
