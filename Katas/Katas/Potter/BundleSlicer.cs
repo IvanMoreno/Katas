141b58e5f7381960b324a95184b779ab3b67a1f0
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 22:03:42 +0200

    refactor: extract methods

--- Katas/Katas/Potter/BundleSlicer.cs
+++ Katas/Katas/Potter/BundleSlicer.cs
@@ -2,22 +2,28 @@ namespace Katas.Potter;
 
 public class BundleSlicer {
     public List<PotterBookBundle> BundleFrom(List<PotterBook> books) {
-        if (books.Count == 1)
+        if (books.Count <= 1)
             return [new PotterBookBundle(books)];
         
+        var bundle = BundleBestDeals(books);
+        var remainingBooks = RemainingBooks(books, bundle);
+        return bundle.Concat(BundleFrom(remainingBooks)).ToList();
+    }
+
+    static List<PotterBook> RemainingBooks(List<PotterBook> books, List<PotterBookBundle> bundle) {
+        var remainingBooks = new List<PotterBook>(books);
+        foreach (var book in bundle.SelectMany(book => book)) {
+            remainingBooks.Remove(book);
+        }
+
+        return remainingBooks;
+    }
+
+    static List<PotterBookBundle> BundleBestDeals(List<PotterBook> books) {
         var bundle = books.Distinct();
         var result = new List<PotterBookBundle> {
             new PotterBookBundle(bundle),
         };
-        
-        var remainingBooks = new List<PotterBook>(books);
-        foreach (var book in result.SelectMany(book => book)) {
-            remainingBooks.Remove(book);
-        }
-        
-        if (remainingBooks.Count > 0)
-            result.AddRange(BundleFrom(remainingBooks));
-        
         return result;
     }
 }
