a533a3cc4fdcdffcda69ca374da2a91d98e6bbbf
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 21:49:26 +0200

    refactor: inline variable

--- Katas/Katas/Potter/BundleSlicer.cs
+++ Katas/Katas/Potter/BundleSlicer.cs
@@ -2,16 +2,15 @@ namespace Katas.Potter;
 
 public class BundleSlicer {
     public List<PotterBookBundle> BundleFrom(List<PotterBook> books) {
-        var result = new List<PotterBookBundle>();
-
         var remainingBooks = new List<PotterBook>(books);
         var bundledBooks = books.Distinct();
         foreach (var book in bundledBooks) {
             remainingBooks.Remove(book);
         }
 
-        result.Add(new PotterBookBundle(bundledBooks));
-        result.Add(new PotterBookBundle(remainingBooks));
-        return result;
+        return [
+            new PotterBookBundle(bundledBooks),
+            new PotterBookBundle(remainingBooks)
+        ];
     }
 }
