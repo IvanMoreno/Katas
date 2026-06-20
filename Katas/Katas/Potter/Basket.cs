8959c62d1402cd621be5891ae062401fcda40150
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 20:24:58 +0200

    refactor: remove unnecessary conditional

--- Katas/Katas/Potter/Basket.cs
+++ Katas/Katas/Potter/Basket.cs
@@ -8,23 +8,17 @@ public class Basket {
     }
 
     public Price Price() {
-        if (books.Count > 1) {
-            var differentBooks = books.Distinct().Count();
-
-            var nonBundledBooksPrice = new Price((books.Count - differentBooks) * 8);
-            var bundlePrice = new Price(differentBooks * 8);
-            
-            var discountedPrice = bundlePrice.Off(DiscountFor(differentBooks));
-            return discountedPrice + nonBundledBooksPrice;
-        }
-
-        return new Price(books.Count * 8);
+        var differentBooks = books.Distinct().Count();
+        var nonBundledBooksPrice = new Price((books.Count - differentBooks) * 8);
+        var bundlePrice = new Price(differentBooks * 8);
+        var discountedPrice = bundlePrice.Off(DiscountFor(differentBooks));
+        return discountedPrice + nonBundledBooksPrice;
     }
 
     static Discount DiscountFor(int differentBooks) {
         if (differentBooks == 3)
             return new Discount(10);
-        
+
         if (differentBooks == 2)
             return new Discount(5);
 
