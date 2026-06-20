20e146dad64a0cfa146f4b35caeb32551c488d1f
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 20:52:10 +0200

    refactor: extract bundle file

---
+++ Katas/Katas/Potter/PotterBookBundle.cs
@@ -0,0 +1,25 @@
+namespace Katas.Potter;
+
+public class PotterBookBundle {
+    static readonly Dictionary<int, Discount> bundleSizeToDiscount = new() {
+        { 0, new Discount(0) },
+        { 1, new Discount(0) },
+        { 2, new Discount(5) },
+        { 3, new Discount(10) },
+        { 4, new Discount(20) },
+        { 5, new Discount(25) }
+    };
+    
+    readonly IEnumerable<PotterBook> books;
+
+    public Price Price => new Price(Size * 8).Off(DiscountFor(Size));
+    public int Size => books.Count();
+
+    public PotterBookBundle(IEnumerable<PotterBook> books) {
+        this.books = books;
+    }
+    
+    static Discount DiscountFor(int bundleSize) {
+        return bundleSizeToDiscount[bundleSize];
+    }
+}
