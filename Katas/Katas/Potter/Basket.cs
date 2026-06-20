a97d4afe4a8c8adbb3b032e2efbcc9016770d800
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-20 20:02:50 +0200

    refactor: extract different books variable

--- Katas/Katas/Potter/Basket.cs
+++ Katas/Katas/Potter/Basket.cs
@@ -19,13 +19,14 @@ public class Basket {
         }
         
         if (books.Count == 3) {
-            var have10Discount = books.Distinct().Count() == 3;
+            var differentBooks = books.Distinct().Count();
+            var have10Discount = differentBooks == 3;
             if (have10Discount) {
                 var bundlePrice = new Price(3 * 8);
                 return bundlePrice.Off(new Discount(10));
             }
             
-            var have5Discount = books.Distinct().Count() == 2;
+            var have5Discount = differentBooks == 2;
             if (have5Discount) {
                 var bundlePrice = new Price(2 * 8);
                 return bundlePrice.Off(new Discount(5)) + new Price(8);
